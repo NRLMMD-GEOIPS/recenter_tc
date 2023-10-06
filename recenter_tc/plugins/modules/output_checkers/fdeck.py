5a161e021f60c0892a27488d3e65436717017658
evrose54 <mrewrose@gmail.com>
2023-10-06 12:37:03 -0600

    updated fdeck output_checker to only define different functions

--- recenter_tc/plugins/modules/output_checkers/fdeck.py
+++ recenter_tc/plugins/modules/output_checkers/fdeck.py
@@ -13,6 +13,7 @@
 """Test script for representative product comparisons."""
 
 import logging
+from geoips.plugins.modules.output_checkers import text
 
 LOG = logging.getLogger(__name__)
 
@@ -40,47 +41,6 @@ def correct_type(fname):
     return False
 
 
-def outputs_match(output_product, compare_product):
-    """Check if two fdeck files match.
+outputs_match = text.outputs_match
 
-    Parameters
-    ----------
-    plugin: OutputCheckerPlugin
-        The corresponding geotiff OutputCheckerPlugin that has access to needed methods
-    output_product : str
-        Full path to current output product
-    compare_product : str
-        Full path to "good" comparison product
-
-    Returns
-    -------
-    bool
-        Return True if products match, False if they differ
-    """
-    LOG.info("Comparing FDECK products.")
-    from geoips.plugins.modules.output_checkers.text import outputs_match
-
-    return outputs_match(output_product, compare_product)
-
-
-def call(plugin, compare_path, output_products):
-    """Compare the "correct" imagery found in comparepath with list of output_products.
-
-    Parameters
-    ----------
-    plugin: OutputCheckerPlugin
-        - The corresponding fdeck OutputCheckerPlugin that has access to needed methods
-    comparepath (str) :
-        - Path to directory of "correct" products - filenames must match
-          output_products
-    output_products (list) :
-        - List of strings of current output products, to compare with products in
-          compare_path
-
-    Returns
-    -------
-    int: Binary code: Good products, bad products, missing products
-    """
-    LOG.info("Using compare_outputs_recenter_tc")
-    retval = plugin.compare_outputs(compare_path, output_products)
-    return retval
+call = text.call
