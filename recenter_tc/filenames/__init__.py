71723a13d2ade78e446bb71d96671dac4926a258
mindyls <13873624+mindyls@users.noreply.github.com>
2023-10-12 13:25:18 -0700

    Update __init__.py

--- recenter_tc/filenames/__init__.py
+++ recenter_tc/filenames/__init__.py
@@ -10,4 +10,4 @@
 # # # for more details. If you did not receive the license, for more information see:
 # # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
 
-"""init file."""
+"""Filename formatter GeoIPS plugins for recenter_tc package."""
