2555b88bd31bf4913994b0d215c3e4de60e4af88
mindyls <13873624+mindyls@users.noreply.github.com>
2023-10-16 06:33:28 -0700

    Add space in log statement

--- recenter_tc/plugins/modules/sector_adjusters/recenter_tc.py
+++ recenter_tc/plugins/modules/sector_adjusters/recenter_tc.py
@@ -389,7 +389,7 @@ def recenter_with_archer(
     out_fnames = []
     if area_def_to_recenter.sector_info["vmax"] < ARCHER_REQUIRED_VMAX_KTS:
         LOG.info(
-            "***SKIPPING not attempting to run archer,"
+            "***SKIPPING not attempting to run archer, "
             "vmax of %s less than required %s kts",
             area_def_to_recenter.sector_info["vmax"],
             ARCHER_REQUIRED_VMAX_KTS,
