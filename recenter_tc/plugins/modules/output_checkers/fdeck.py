90bd69ff4765b8ffc3b15b7a36bf24710ef4cad6
Evan Rose <62626811+evrose54@users.noreply.github.com>
2023-10-17 14:49:15 -0600

    updated recenter_tc to use output_checkers interface (#26)
    * updated recenter_tc to use output_checkers interface
    
    * added docs for recenter tc
    
    * added v1_11_5a0 to the index.rst
    
    * final updates for fdeck_checker with new output checkers interface
    
    * changed comment to fdeck instead of geotiff
    
    * running checks again
    
    * Move release notes to 1.11.7a0
    
    * refactoring updates for output_checkers
    
    * changed index.rst to include v1_11_7 instead of v1_11_5
    
    * updated fdeck output_checker to only define different functions
    
    * changed correct_type to correct_file_format
    
    * Update output checker for flake8
    
    ---------
    
    Co-authored-by: mindyls <mindyls@users.noreply.github.com>

---
+++ recenter_tc/plugins/modules/output_checkers/fdeck.py
@@ -0,0 +1,48 @@
+# # # Distribution Statement A. Approved for public release. Distribution unlimited.
+# # #
+# # # Author:
+# # # Naval Research Laboratory, Marine Meteorology Division
+# # #
+# # # This program is free software: you can redistribute it and/or modify it under
+# # # the terms of the NRLMMD License included with this program. This program is
+# # # distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+# # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
+# # # for more details. If you did not receive the license, for more information see:
+# # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
+
+"""Test script for representative product comparisons."""
+
+import logging
+from geoips.plugins.modules.output_checkers import text
+
+LOG = logging.getLogger(__name__)
+
+interface = "output_checkers"
+family = "standard"
+name = "fdeck"
+
+
+def correct_file_format(fname):
+    """Check if fname is an fdeck file.
+
+    Parameters
+    ----------
+    fname: str
+        Name of file to check.
+
+    Returns
+    -------
+    bool: boolean
+        True if it is an fdeck file (``*_FIX``), False otherwise.
+    """
+    if fname.split("_")[-1] in ["FIX"]:
+        with open(fname) as f:
+            line = f.readline()
+        if isinstance(line, str):
+            return True
+    return False
+
+
+outputs_match = text.outputs_match
+
+call = text.call
