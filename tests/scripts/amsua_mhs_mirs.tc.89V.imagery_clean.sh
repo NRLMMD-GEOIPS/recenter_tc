00f8866c946b55bef4e976fdc5a238f7374b9b74
mindyls <13873624+mindyls@users.noreply.github.com>
2025-04-16 20:43:45 +0000

    1.15.2 release NRLMMD-GEOIPS/geoips#901

--- tests/scripts/amsua_mhs_mirs.tc.89V.imagery_clean.sh
+++ tests/scripts/amsua_mhs_mirs.tc.89V.imagery_clean.sh
@@ -5,9 +5,9 @@
 run_procflow \
     $GEOIPS_TESTDATA_DIR/test_data_amsub/data/NPR-MIRS-IMG_v11r4_ma2_s202104192335000_e202104190118000_c202104200206490.nc \
     --procflow single_source \
-    --reader_name amsub_mirs \
+    --reader_name amsua_mhs_mirs \
     --product_name 89V \
-    --compare_path "$GEOIPS_PACKAGES_DIR/recenter_tc/tests/outputs/amsub_mirs.tc.89V.imagery_clean" \
+    --compare_path "$GEOIPS_PACKAGES_DIR/recenter_tc/tests/outputs/amsua_mhs_mirs.tc.89V.imagery_clean" \
     --output_formatter imagery_clean \
     --filename_formatter tc_clean_fname \
     --sector_adjuster recenter_tc \
