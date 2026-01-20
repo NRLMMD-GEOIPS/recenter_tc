76ed4579c0c2b3a39f00273ac40d011cb1a165b5
Gwyn Uttmark <biosafetylevel5@gmail.com>
2026-01-20 15:14:18 -0700

    Update files to 2026 black standards

--- recenter_tc/filenames/base_paths.py
+++ recenter_tc/filenames/base_paths.py
@@ -8,7 +8,6 @@ environment variable GEOIPS_OUTDIRS.  Individual GEOIPS_OUTDIRS
 relative paths can be overridden by setting appropriate environment variables.
 """
 
-
 # Python Standard Libraries
 import logging
 from os import getenv
