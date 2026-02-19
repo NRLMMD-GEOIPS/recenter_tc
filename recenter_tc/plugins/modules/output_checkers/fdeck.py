fb6056cee8c9989ae8988e6fa91b34e5bb07ded4
BSL-5 <biosafetylvl5@users.noreply.github.com>
2026-02-19 09:24:49 -0700

    add no qa for call import as this function IS used

--- recenter_tc/plugins/modules/output_checkers/fdeck.py
+++ recenter_tc/plugins/modules/output_checkers/fdeck.py
@@ -5,7 +5,7 @@
 
 import logging
 
-from geoips.plugins.modules.output_checkers.text import outputs_match, call
+from geoips.plugins.modules.output_checkers.text import outputs_match, call # noqa: F401
 from geoips.geoips_utils import get_numpy_seeded_random_generator
 
 LOG = logging.getLogger(__name__)
