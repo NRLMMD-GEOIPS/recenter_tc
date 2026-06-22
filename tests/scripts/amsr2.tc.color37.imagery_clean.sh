419ec7dd4cdfd79f2c4803942f77d1ef2b8e261b
Kyle Thompson <kyletranthompson@gmail.com>
2026-06-22 09:10:49 -0700

    archer kwargs and sector

--- tests/scripts/amsr2.tc.color37.imagery_clean.sh
+++ tests/scripts/amsr2.tc.color37.imagery_clean.sh
@@ -16,6 +16,7 @@ geoips run single_source \
     --trackfile_parser bdeck_parser \
     --trackfiles $GEOIPS_PACKAGES_DIR/geoips/tests/sectors/tc_bdecks/bio012020.dat \
     --sector_adjuster recenter_tc \
+    --sector_adjuster_kwargs '{"include_archer_info": True}' \
     --sector_adjuster_kwargs '{
         "archer_config": {
             "include_archer_metadata_in_sector_info": True,
